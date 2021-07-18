b89e42b7db7722dc381c7056e29ef10ca68c8644
Beñat Garcia <benat.dev@gmail.com>
2021-07-18 10:36:40 +0200

    Esttructura de header y footer y página Inicio

---
+++ app/Views/inicio.php
@@ -0,0 +1,20 @@
+<?= $this->extend('app') ?>
+
+<?= $this->section('content') ?>
+
+<section>
+	<!--?= $this->renderSection('content') ?-->
+	<button href="./tiempo" class="">
+
+		<h1>El tiempo</h1>
+
+	</button>
+
+	<button href="./noticias" class="">
+
+		<h1>Las noticias</h1>
+
+	</button>
+
+</section>
+<?= $this->endSection() ?>
